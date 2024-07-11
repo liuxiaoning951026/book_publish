aa8eab4504e9286f7f4fdddec8071b554da83a7a
liuning <liuning11107@navinfo.com>
2024-07-11 13:59:44 +0800

    fix(系统):  引入element-ui库

--- book_publish/src/components/HelloWorld.vue
+++ book_publish/src/components/HelloWorld.vue
@@ -1,5 +1,24 @@
 <template>
-  <div class="hello">你好</div>
+  <div class="hello">
+    <el-descriptions
+      title="垂直带边框列表"
+      direction="vertical"
+      :column="4"
+      border
+    >
+      <el-descriptions-item label="用户名">kooriookami</el-descriptions-item>
+      <el-descriptions-item label="手机号">18100000000</el-descriptions-item>
+      <el-descriptions-item label="居住地" :span="2"
+        >苏州市</el-descriptions-item
+      >
+      <el-descriptions-item label="备注">
+        <el-tag size="small">学校</el-tag>
+      </el-descriptions-item>
+      <el-descriptions-item label="联系地址"
+        >江苏省苏州市吴中区吴中大道 1188 号</el-descriptions-item
+      >
+    </el-descriptions>
+  </div>
 </template>
 
 <script>
